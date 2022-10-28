7d2021321d871c62248af578ce6a9a8d28864e8f
Otto Palmgren <protpa@linux-desktop7.tuni.fi>
2022-10-28 12:16:51 +0300

    testi inva2

--- wk08/iterator_invalidation/invalidation2.cc
+++ wk08/iterator_invalidation/invalidation2.cc
@@ -11,9 +11,9 @@ using namespace std;
  * @param vec vector where every second item is erased.
  */
 void eraseEverySecond(std::vector<int>& vec) {
-    auto beg = vec.begin();
-    auto end = vec.end();
-    for (auto i = beg; i != end; i = i+2)
-    { vec.erase(i); }
+    for(auto it = next(vec.begin(),1);it != vec.end();){
+        it=vec.erase(it);
+        if(it!=vec.end()){it++;}
+    }
 }
 
